e50c188d073336524a15035d9de13f6d889dfcc7
Gabriel <gabrielcemp@gmail.com>
2021-07-05 21:13:06 -0300

    add create & enter room

---
+++ src/pages/Home.tsx
@@ -0,0 +1,67 @@
+import '../styles/auth.scss'
+import illustrationImg from '../assets/illustration.svg'
+import logoImg from '../assets/logo.svg'
+import googleIconImg from '../assets/google-icon.svg'
+import { useHistory } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
+import { FormEvent, useState } from 'react'
+import { Button } from '../components/Button'
+import { database } from '../services/firebase'
+
+export function Home() {
+  const [roomCode, setRoomCode] = useState('')
+
+  const { user, signInWithGoogle } = useAuth()
+  const history = useHistory()
+
+  async function handleCreateRoom() {
+    if (!user) await signInWithGoogle()
+    history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if (roomCode.trim() === '') return
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists!')
+      return
+    }
+
+    history.push(`/rooms/${roomCode}`)
+  }
+
+  return (
+    <div id='page-auth'>
+      <aside>
+        <img src={illustrationImg} alt="Illustration symbolizing questions and answers" />
+        <strong>Create live Q&amp;A rooms</strong>
+        <p>Answer your audience's questions in real time</p>
+      </aside>
+      <main>
+        <div className='main-content'>
+          <img src={logoImg} alt="Let me ask" />
+          <button onClick={handleCreateRoom} className='create-room'>
+            <img src={googleIconImg} alt="Google logo" />
+            Create your room with google
+          </button>
+          <div className='separator'>Or enter a room</div>
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Enter the room code"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
+            <Button type='submit'>
+              Enter the room
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,59 @@
+import '../styles/auth.scss'
+import illustrationImg from '../assets/illustration.svg'
+import logoImg from '../assets/logo.svg'
+import { FormEvent, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+import { Button } from '../components/Button'
+
+export function NewRoom() {
+  const [newRoom, setNewRoom] = useState('')
+  
+  const { user } = useAuth()
+  const history = useHistory()
+
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if (newRoom.trim() === '') return
+
+    const roomRef = database.ref('rooms')
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id
+    })
+
+    history.push(`/rooms/${firebaseRoom.key}`)
+  }
+
+  return (
+    <div id='page-auth'>
+      <aside>
+        <img src={illustrationImg} alt="Illustration symbolizing questions and answers" />
+        <strong>Create live Q&amp;A rooms</strong>
+        <p>Answer your audience's questions in real time</p>
+      </aside>
+      <main>
+        <div className='main-content'>
+          <img src={logoImg} alt="Let me ask" />
+          <h2>Create a new room</h2>         
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Room name"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
+            />
+            <Button type='submit'>
+              Create room
+            </Button>
+          </form>
+          <p>Want to join an existing room? <Link to="/">Click here!</Link></p>
+        </div>
+      </main>
+    </div>
+  )
+}
